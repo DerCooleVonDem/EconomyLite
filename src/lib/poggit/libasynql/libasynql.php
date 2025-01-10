b86e245472f793210e89832aa7c6a9f740a8f7ad
Jonas <jonaswindmann@gmail.com>
2025-01-10 10:47:00 +0100

    Fixed poggit lint

--- src/lib/poggit/libasynql/libasynql.php
+++ src/lib/poggit/libasynql/libasynql.php
@@ -24,6 +24,7 @@ namespace DerCooleVonDem\EconomyLite\lib\poggit\libasynql;
 
 use InvalidArgumentException;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use pocketmine\utils\Terminal;
 use pocketmine\utils\Utils;
 use DerCooleVonDem\EconomyLite\lib\poggit\libasynql\base\DataConnectorImpl;
@@ -55,7 +56,7 @@ final class libasynql{
 		self::$packaged = __CLASS__ !== 'poggit\libasynql\libasynql';
 
 		if(!self::$packaged && defined("pocketmine\\VERSION")){
-			echo Terminal::$COLOR_YELLOW . "Warning: Use of unshaded libasynql detected. Debug mode is enabled. This may lead to major performance drop. Please use a shaded package in production. See https://poggit.pmmp.io/virion for more information.\n";
+			Server::getInstance()->getLogger()->warning("Warning: Use of unshaded libasynql detected. Debug mode is enabled. This may lead to major performance drop. Please use a shaded package in production. See https://poggit.pmmp.io/virion for more information.\n");
 		}
 	}
 
